dc7edee52a3db2043026368fabf88bfeecc247c5
DEVJS2005 <79548150+DEVJS2005@users.noreply.github.com>
2022-06-17 23:36:04 -0300

    Piloto

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookAPI.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string author { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+
+    }
+}
